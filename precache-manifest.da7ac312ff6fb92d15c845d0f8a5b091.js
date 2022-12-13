49a210522a7c4293cd061ab5a10d379132abeddf
Krista Cannady <kristalcannady@gmail.com>
2022-12-13 15:48:00 -0500

    Updates

---
+++ precache-manifest.da7ac312ff6fb92d15c845d0f8a5b091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c928cbc3ae8c28706afa8835abbf8464",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "3a56956ff2a0be12564b",
+    "url": "/React-Portfolio/static/css/main.2b709347.chunk.css"
+  },
+  {
+    "revision": "7ec12f437a2f4c02f60c",
+    "url": "/React-Portfolio/static/js/2.2877ad6c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/React-Portfolio/static/js/2.2877ad6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7665912d4d4e1a15410e",
+    "url": "/React-Portfolio/static/js/3.225ad2e0.chunk.js"
+  },
+  {
+    "revision": "3a56956ff2a0be12564b",
+    "url": "/React-Portfolio/static/js/main.414661b5.chunk.js"
+  },
+  {
+    "revision": "0b06d080eb5335afc121",
+    "url": "/React-Portfolio/static/js/runtime-main.c77ae543.js"
+  }
+]);
